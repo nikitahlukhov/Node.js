0105a83b1af8fe3ba5d5af79864207d06463731d
Nikita Hlukhov <zholud92@gmail.com>
2020-03-18 19:19:21 +0200

    commit

--- test.js
+++ test.js
@@ -2,20 +2,24 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
     let reqObj = {
         method: req.method,
         url: req.url,
         time: Date.parse(new Date()),
     }
+
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+    
     fs.readFile('test.json', 'utf8', function (err, data) {
         if (err) {
             let obj = {
                 logs: [reqObj],
             };
+
             fs.writeFileSync('test.json', JSON.stringify(obj))
+
             res.end(JSON.stringify({
                 status: res.statusMessage
             }))
@@ -25,7 +29,9 @@ const server = http.createServer(function (req, res) {
             } else {
                 let obj = JSON.parse(data);
                 obj.logs.push(reqObj);
+
                 fs.writeFileSync('test.json', JSON.stringify(obj))
+                
                 res.end(JSON.stringify({
                     status: res.statusMessage
                 }))
