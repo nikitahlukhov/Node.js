7b1b5a9fc3a3358469fc401d0806dbc5af078a15
Nikita Hlukhov <zholud92@gmail.com>
2020-03-29 11:38:15 +0300

    prep

--- auth/auth.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const secret = require('./secret').secret;
-
-module.exports = (req, res, next) => {
-    const [jwt_token] = req.headers['authorization'].split(' ');
-    let user = jwt.verify(jwt_token, secret);
-    req.user = user;
-    next();   
-};
